75811d003ec212c418240c0d1e825d38a07d3e9e
AleksHodur <aleksandra.hodur.jaromin@gmail.com>
2025-02-07 18:10:10 +0100

    Styling the project list

--- src/components/projectList/ProjectList.js
+++ src/components/projectList/ProjectList.js
@@ -1,15 +1,33 @@
+import Avatar from '../avatar/Avatar';
 import './ProjectList.css';
-import { useCollection } from '../../hooks/useCollection';
+import { Link } from 'react-router-dom';
+import { timestamp } from '../../firebase/config';
 
 function ProjectList({ projects}) {
+    const convertDate = (date) => {
+        const dateJS = date.toDate();
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+        return dateJS.toLocaleDateString('en-GB', options);
+    }
 
     return ( 
-        <div>
+        <div className='project-list'>
             {projects.length === 0 && <p>No projects yet!</p>}
             {projects.map((project) => (
-                <div key={project.id} className="">
-                    { project.name }
-                </div>
+                <Link to={`/projects/${project.id}`} key={ project.id }>
+                    <h4>{ project.name }</h4>
+                    <p>Due by { convertDate(project.dueDate) }</p>
+                    <div className="assigned-to">
+                        <ul>
+                            {project.assignedUsersList.map(user => (
+                                <li key={user.photoURL}>
+                                    <Avatar src={user.photoURL} />
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </Link>
             ))}
         </div>
      );
