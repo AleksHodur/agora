b675628d47ee97b03a73d73b49634720e236ea95
AleksHodur <aleksandra.hodur.jaromin@gmail.com>
2025-02-11 15:29:09 +0100

    Dashboard's filter design

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import './Dashboard.css';
 import { useCollection } from '../../hooks/useCollection';
 import ProjectList from '../../components/projectList/ProjectList';
+import ProjectFilter from '../../components/projectFilter/ProjectFilter';
 
 function Dashboard () {
     const { documents, error } = useCollection('projects');
@@ -9,6 +10,7 @@ function Dashboard () {
         <div>
             <h2 className="page-title">Dashboard</h2>
             {error && <p className='error'>{ error }</p>}
+            {documents && <ProjectFilter/>}
             {documents && <ProjectList projects={documents}/>}
         </div>
      );
