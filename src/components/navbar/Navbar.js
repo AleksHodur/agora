fee9b54f702b854bc0ed2ed3cb1a65424ea277b2
AleksHodur <aleksandra.hodur.jaromin@gmail.com>
2025-04-24 10:34:13 +0200

    Deleting logout and links from Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,9 @@
-import { Link } from 'react-router-dom';
-import { useLogout } from '../../hooks/useLogout'
 import { useAuthContext } from '../../hooks/useAuthContext';
 
 import './Navbar.css';
 import Parthenon from '../../assets/parthenon.svg';
 
 function Navbar () {
-    const { logout, isPending } = useLogout();
     const { user } = useAuthContext();
 
     return ( 
@@ -17,15 +14,7 @@ function Navbar () {
                     <span>Agora</span>
                 </li>
 
-                {user == null && 
-                    <>
-                        <li><Link to="/login">Login</Link></li>
-                        <li><Link to="/signup">Signup</Link></li>
-                    </>
-                }
-                {(user != null && !isPending) && <button className="btn" onClick={logout}>Logout</button>}
-
-                {(user != null && isPending) && <button className="btn" disabled>Logging out...</button>}
+                <button className="btn" >Logout</button>
             </ul>
         </div>
      );
