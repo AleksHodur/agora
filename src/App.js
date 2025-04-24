bbb3f1113b532c34a71f24ef7c277dff3e0bd644
AleksHodur <aleksandra.hodur.jaromin@gmail.com>
2025-04-24 10:33:26 +0200

    New App.js: deleting login and signup, deleting authContext and user confirmation

--- src/App.js
+++ src/App.js
@@ -4,57 +4,39 @@ import { BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 
 import Dashboard from './pages/dashboard/Dashboard';
 import Create from './pages/create/Create';
-import Login from './pages/login/Login';
-import Signup from './pages/signup/Signup';
 import Project from './pages/project/Project';
 import Navbar from './components/navbar/Navbar';
 import Sidebar from './components/sidebar/Sidebar';
-import { useAuthContext } from './hooks/useAuthContext';
 import OnlineUsers from './components/onlineUsers/OnlineUsers';
 
 function App() {
-
-  const { user } = useAuthContext();
 
   return (
     <div className="App">
       <BrowserRouter>
 
-        { user && <Sidebar />}
+        <Sidebar />
 
         <div className="container">
           <Navbar />
           <Switch>
 
             <Route exact path='/'>
-                { !user && <Redirect to="/login"/>}
-                { user && <Dashboard/>}
+                <Dashboard/>
             </Route>
 
               <Route path='/create'>
-                { !user && <Redirect to="/login"/>}
-                { user && <Create/>}
+                <Create/>
               </Route>
 
               <Route path='/project/:id'>
-                { !user && <Redirect to="/login"/>}
-                { user && <Project/>}
-              </Route>
-
-              <Route path='/login'>
-                { !user && <Login/>}
-                { user && <Redirect to="/" />}
-              </Route>
-
-              <Route path='/signup'>
-              { !user && <Signup/>}
-              { user && <Redirect to="/" />}
+                <Project/>
               </Route>
 
           </Switch>
         </div>
 
-        { user && <OnlineUsers />}
+        <OnlineUsers />
       </BrowserRouter>
     </div>
   );
